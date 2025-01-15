c23a45a5c07a7368c79626e8443110421d171c9f
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2025-01-15 22:49:22 +0300

    Update user.py

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from app.data import schemas 
+from app.data import schemas
 from app.data.models import User
 from app.utils.auth import create_user, authenticate_user
 from app.utils.security import verify_password
@@ -10,12 +10,13 @@ router = APIRouter(prefix="/user", tags=["User"])
 
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
+
 @router.post("/create")
 async def registration_user(request: schemas.RequestCreateUser) -> schemas.ResponseUserLogIn:
     user = await create_user(request)
     token = await authenticate_user(data={"sub": request.username})
-    
-    redis.set(token, user.username, ex=int(ACCESS_TOKEN_EXPIRE_MINUTES_REDIS))
+
+    await redis.set(token, user.username, ex=int(ACCESS_TOKEN_EXPIRE_MINUTES_REDIS))
 
     return schemas.ResponseUserLogIn(
         user_token=token
@@ -27,10 +28,10 @@ async def log_in_user(request: schemas.RequestLogInUser) -> schemas.ResponseUser
     user = await User.find_one(User.username == request.username)
     if not user or not verify_password(request.password, user.hashed_password):
         raise HTTPException(status_code=404, detail="Incorrect email or password")
-    
+
     token = await authenticate_user(data={"sub": request.username})
-    
-    redis.set(token, user.username, ex=int(ACCESS_TOKEN_EXPIRE_MINUTES_REDIS))
+
+    await redis.set(token, user.username, ex=int(ACCESS_TOKEN_EXPIRE_MINUTES_REDIS))
 
     return schemas.ResponseUserLogIn(
         user_token=token
