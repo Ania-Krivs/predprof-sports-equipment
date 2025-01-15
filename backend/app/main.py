bd1720a60527b2864debc2abc2c388a98b252998
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2025-01-15 22:52:00 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.middleware.cors import CORSMiddleware
 
-from app import MONGO_DSN, ENVIRONMENT, projectConfig
-from app.routers import system, user
+from backend.app import MONGO_DSN, ENVIRONMENT, projectConfig
+from backend.app.routers import system, user, inventory
 
 if ENVIRONMENT == "prod":
     app = FastAPI(
@@ -23,6 +23,8 @@ else:
 
 app.include_router(system.router)
 app.include_router(user.router)
+app.include_router(inventory.inventory_router)
+
 
 @app.on_event('startup')
 async def startup_event():
@@ -32,7 +34,8 @@ async def startup_event():
         database=client['Predprof'],
         document_models=Document.__subclasses__() + UnionDoc.__subclasses__()
     )
-    
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
