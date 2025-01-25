8eff0a05e502e7c40987677d9b4cc112938617fd
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2025-01-25 16:14:47 +0300

    Update inventory.py

--- backend/app/routers/inventory.py
+++ backend/app/routers/inventory.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
 from app.data.models import Inventory, User
 from app.data.schemas import SInventoryId, SInventoryUpdateData, SInventoryAddData
 from datetime import datetime
 from app.exceptions import InventoryAlreadyExisted
+from fastapi import APIRouter, File, Form
+from typing_extensions import Annotated
+from app.utils.image import process_image
+from app.constants import default_image
 
 inventory_router = APIRouter(prefix="/inventory", tags=["Inventory"])
 
@@ -20,6 +23,7 @@ async def update_inventory_by_id(update_data: SInventoryUpdateData) -> dict:
     inventory.name = update_data.name
     inventory.amount = update_data.amount
     inventory.state = update_data.state
+    inventory.description = update_data.description
 
     if update_data.user_id not in inventory.used_by_user_ids:
         inventory.used_by_user_ids.append(update_data.user_id)
@@ -27,20 +31,41 @@ async def update_inventory_by_id(update_data: SInventoryUpdateData) -> dict:
     inventory.updated_at = datetime.now()
 
     await inventory.save()
-    return {"ok": True}
+    return {"ok": True, "id": update_data.id}
+
+
+@inventory_router.patch("/update_inventory_image")
+async def update_inventory_image(inventory_id: SInventoryId, file: Annotated[bytes, File()],
+                                 extension: Annotated[str, Form()] = "png") -> dict:
+    image = await process_image(file, extension)
+
+    inventory = await Inventory.find_one(Inventory.id == inventory_id.id)
+    inventory.image = image["image_link"]
+    await inventory.save()
+
+    return {"ok": True, "image_link": image["image_link"], "file_name": f"{image['file_name']}.{extension}",
+            "id": inventory.id}
 
 
 @inventory_router.post("/add_inventory")
-async def add_inventory(add_data: SInventoryAddData) -> dict:
+async def add_inventory(add_data: SInventoryAddData, file: Annotated[bytes, File(), None] = None,
+                        extension: Annotated[str, Form()] = "png") -> dict:
     if await Inventory.find_one(add_data.name == Inventory.name):
         raise InventoryAlreadyExisted
 
+    if not file:
+        image = default_image
+    else:
+        image = (await process_image(file, extension))["image_link"]
+
     inventory = Inventory(name=add_data.name, amount=add_data.amount, used_by_user_ids=[], state=add_data.state,
+                          descriprion=add_data.description,
+                          image=image,
                           updated_at=datetime.now(),
                           created_at=datetime.now())
 
     await inventory.create()
-    return {"ok": True}
+    return {"ok": True, "id": (await Inventory.find_one(add_data.name == Inventory.name)).id}
 
 
 @inventory_router.get("/all_inventory/")
